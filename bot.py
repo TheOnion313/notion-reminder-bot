139c06fd20d8b434a20dab1c3555be8a43cd6303
TheOnion313 <royn77777@gmail.com>
2020-10-18 17:49:29 +0300

    added backup() and load() functions on top of backup.p file, ignored from commits. also fixed remove bug to handle illegitamate IDs

--- bot.py
+++ bot.py
@@ -3,10 +3,11 @@ from datetime import datetime
 
 from discord.ext import commands, tasks
 
+import pickle
 
 def add_notification(send_time: str, message: str, channel_id: int):
     send_hours, send_minutes = [int(i) for i in send_time.split(':')]
-    notifcations.append(
+    notifications.append(
         {'send_hour': send_hours, 'send_minute': send_minutes, 'message': message, 'target_channel_id': channel_id,
          'time_stamp': int(time.time())})
 
@@ -17,17 +18,24 @@ def to_str(n: dict):
            f'Message content: "{n["message"]}"\n' \
            f'Time stamp: {n["time_stamp"]}'
 
+def backup():
+    with open('backup.p', 'wb') as f:
+        pickle.dump(notifications, f)
+
+def load():
+    global notifications
+    with open('backup.p', 'rb') as f:
+        notifications = pickle.load(f)
+        backup()
 
 bot = commands.Bot('--')
-
-notifcations = []
-
+load()
 
 @tasks.loop(seconds=60)
 async def notify():
     await bot.wait_until_ready()
     time = datetime.now()
-    for n in notifcations:
+    for n in notifications:
         if time.hour == n['send_hour'] and time.minute == n['send_minute']:
             message_channel = bot.get_channel(n['target_channel_id'])
             print(f"Got channel {message_channel}")
@@ -55,21 +63,28 @@ async def add(ctx, given_name=None, time=None, message=None):
         await ctx.send('Time format invalid.')
         return
     add_notification(time, message, int(channel_id))
-    await ctx.send(f'Added notification:\n{to_str(notifcations[-1])}')
+    await ctx.send(f'Added notification:\n{to_str(notifications[-1])}')
+    backup()
 
 
 @bot.command()
 async def remove(ctx, time_stamp):
-    for n in notifcations:
-        if n['time_stamp'] == int(time_stamp):
-            notifcations.remove(n)
-            await ctx.send(f'Removed notification\n{to_str(n)}')
+    for i in time_stamp:
+        if i not in '1234567890':
+            await ctx.send(f'"{time_stamp}" is not an integer, not to mention not a valid ID.')
             return
+    for n in notifications:
+        if n['time_stamp'] == int(time_stamp):
+            notifications.remove(n)
+            await ctx.send(f'Removed notification\n{to_str(n)}')
+            backup()
+            return
+    await ctx.send(f'There is no notification with the time stamp {time_stamp}')
 
 @bot.command()
 async def list(ctx):
     msg = ''
-    for n in notifcations:
+    for n in notifications:
         msg += to_str(n)+'\n\n'
 
     if len(msg) == 0:
